7379ce9763d01b9f3b9f78ee921512434b3ce52d
Chilli Mahendra <chillilauren@live.co.uk>
2020-07-21 12:23:16 +0100

    Fix test - throw error if name is missing

--- ViewingsApp/Services/BookingValidator.cs
+++ ViewingsApp/Services/BookingValidator.cs
@@ -14,11 +14,22 @@ namespace ViewingsApp.Services
     {
         public BookingValidation ValidateBooking(BookingRequest bookingRequest, IEnumerable<Agent> allAgents, IEnumerable<Property> allProperties)
         {
+            // If Name in bookingRequest is empty IsValid becomes false
+            if (bookingRequest.Name == "")
+            {
+                return new BookingValidation
+                {
+                    IsValid = false,
+                    ErrorMessage = "You must provide a name."
+                };
+            }
+            // If Name is not empty
             return new BookingValidation
             {
                 IsValid = true,
                 ErrorMessage = ""
             };
+            
         }
     }
 }
